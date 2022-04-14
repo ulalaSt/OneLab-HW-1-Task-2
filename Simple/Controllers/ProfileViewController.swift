67edbfffb00431e505ff3595c349406bff69365d
Ulan Seitkali <ulan.seitkali@nu.edu.kz>
2022-04-14 23:24:32 +0600

    Last commit

--- Simple/Controllers/ProfileViewController.swift
+++ Simple/Controllers/ProfileViewController.swift
@@ -16,15 +16,20 @@ class ProfileViewController: UIViewController {
         table.register(GeneralTableViewCell.self, forCellReuseIdentifier: GeneralTableViewCell.identifier)
         return table
     }()
+    
     private var imageV = UIImageView(image: UIImage(named: ProfileViewModel.data.imageName))
+    
     private var newImageV = UIImageView(image: UIImage(named: "newImage"))
+    
     private var imageBG = UIImageView(image: UIImage(named: ProfileViewModel.data.imageName))
+    
     private var nameLabel: UILabel = {
         let label = UILabel()
         label.textColor = UIColor(red: 0.217, green: 0.217, blue: 0.217, alpha: 1)
         label.font = UIFont(name: "Montserrat-SemiBold", size: 19)
         return label
     }()
+    
     private var emailLabel: UILabel = {
         let label = UILabel()
         label.font = UIFont(name: "Montserrat-Regular", size: 16)
@@ -36,6 +41,7 @@ class ProfileViewController: UIViewController {
         let view = UIView()
         return view
     }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .secondarySystemBackground
@@ -44,15 +50,20 @@ class ProfileViewController: UIViewController {
         tableView.dataSource = self
         modifyHeader()
     }
+    
     private func modifyHeader() {
         headerV.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 200)
+        headerV.addSubview(imageBG)
         headerV.addSubview(imageV)
+        let tapOnProfileImage = UITapGestureRecognizer(target: self, action: #selector(didTapChangePicture))
+        headerV.isUserInteractionEnabled = true
+        imageV.isUserInteractionEnabled = true
+        imageV.addGestureRecognizer(tapOnProfileImage)
         imageV.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(20)
             make.centerX.equalToSuperview()
             make.size.equalTo(100)
         }
-        headerV.addSubview(imageBG)
         
         imageBG.snp.makeConstraints { make in
             make.centerY.equalTo(imageV)
@@ -83,13 +94,16 @@ class ProfileViewController: UIViewController {
         }
         
         tableView.tableHeaderView = headerV
+        tableView.tableHeaderView?.isUserInteractionEnabled = true
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         navigationItem.backButtonTitle = ""
         if let navBar = navigationController?.navigationBar as? GeneralNavigationBar {
             navBar.backLabel.text = "Профиль"
         }
     }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.frame
@@ -103,6 +117,9 @@ class ProfileViewController: UIViewController {
         imageV.layer.borderColor = UIColor.white.cgColor
         imageV.layer.borderWidth = 3
         imageV.layer.masksToBounds = true
+    }
+    @objc func didTapChangePicture(){
+        presentPhotoActionSheet()
     }
 }
 
@@ -132,5 +149,46 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
         case .logout:
             print("Logged Out")
         }
+    }
+}
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func presentPhotoActionSheet(){
+        let actionSheet = UIAlertController(title: "Фото профиля", message: "Как бы вы хотели выбрать свою фотографию?", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Отмена", style: .cancel, handler: nil))
+        actionSheet.addAction(UIAlertAction(title: "Фотографировать", style: .default, handler: { [weak self]_ in
+            self?.presentCamera()
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Выбрать фото", style: .default, handler: { [weak self]_ in
+            self?.presentGallery()
+        }))
+        if let popoverController = actionSheet.popoverPresentationController {
+            popoverController.sourceView = self.view //to set the source of your alert
+            popoverController.sourceRect = CGRect(x: self.view.bounds.midX, y: self.view.bounds.midY, width: 0, height: 0) // you can set this as per your requirement.
+            popoverController.permittedArrowDirections = [] //to hide the arrow of any particular direction
+        }
+        present(actionSheet, animated: true)
+
+    }
+    func presentCamera(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .camera
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc,animated: true)
+    }
+    func presentGallery(){
+        let vc = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc,animated: true)
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        imageV.image = image
+        picker.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
